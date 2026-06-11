21fcf288f75bf48bf755b5fb8f48a940ee877b79
thanhtungtav4 <nguyenthanhtung74119@gmail.com>
2026-06-11 11:50:30 +0700

    style: lower bounce scroll indicator to bottom-6

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -61,7 +61,7 @@ export function HeroSection() {
         </div>
 
         {/* Scroll indicator */}
-        <div className="absolute bottom-16 left-1/2 -translate-x-1/2 animate-bounce">
+        <div className="absolute bottom-6 left-1/2 -translate-x-1/2 animate-bounce">
           <div className="w-8 h-12 rounded-full border-2 border-white/30 flex items-start justify-center p-2">
             <div className="w-1.5 h-3 bg-white/60 rounded-full animate-pulse" />
           </div>
